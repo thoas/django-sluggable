7188b234f74a4df71140e613cc747a2a5f7ed6ce
Florent Messa <florent.messa@gmail.com>
2013-05-16 10:11:29 +0200

    Add more assertions

--- sluggable/tests/__init__.py
+++ sluggable/tests/__init__.py
@@ -7,14 +7,31 @@ class SluggableTests(TestCase):
     def test_simple_add(self):
         poll = Poll.objects.create(question='Quick test')
 
-        self.assertEquals(unicode(poll.slug), u'quick-test')
+        self.assertEquals(poll.slug, u'quick-test')
 
         self.assertEquals(PollSlug.objects.count(), 1)
 
-        slug = PollSlug.objects.get(pk=1)
+        slug = PollSlug.objects.get(slug='quick-test')
 
         self.assertEquals(slug.slug, 'quick-test')
 
         self.assertFalse(slug.redirect)
 
         self.assertEquals(slug.content_object, poll)
+
+        poll.question = 'Another test'
+        poll.save()
+
+        self.assertEquals(poll.slug, u'quick-test')
+
+        poll.slug = 'quick-test-2'
+        poll.save()
+
+        self.assertEquals(PollSlug.objects.count(), 2)
+
+        slug = PollSlug.objects.get(slug='quick-test-2')
+
+        self.assertFalse(slug.redirect)
+
+        slug = PollSlug.objects.get(slug='quick-test')
+        self.assertTrue(slug.redirect)
